0e3ba1419f13f65e555e01b9867816102a9f7d30
jose-acosta-git <jose.acosta.git@gmail.com>
2023-10-31 22:38:07 -0300

    Add @RequestBody tag in controllers create endpoints

--- src/main/java/scooters/controllers/ScootersController.java
+++ src/main/java/scooters/controllers/ScootersController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +25,7 @@ public class ScootersController {
 	private ScootersService scootersService;
 	
 	@PostMapping
-	public Scooter create(ScooterDto dto) {
+	public Scooter create(@RequestBody ScooterDto dto) {
 		return scootersService.save(dto);
 	}
 	
--- src/main/java/scooters/controllers/StopsController.java
+++ src/main/java/scooters/controllers/StopsController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +25,7 @@ public class StopsController {
 	private StopsRepository stopsRepository;
 	
 	@PostMapping
-	public Stop create(StopDto dto) {
+	public Stop create(@RequestBody StopDto dto) {
 		return stopsService.save(dto);
 	}
 	
